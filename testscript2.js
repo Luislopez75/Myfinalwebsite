694d68a646d6d7b2cf24e8ad3fee8fa4a880bc97
Luislopez75 <64113910+Luislopez75@users.noreply.github.com>
2020-05-06 13:06:28 -0400

    More changes
    I did more changes mainly the "contract 1" page, I created my javascript file to do the mouse over etc. stuff for my text for "Contract 1" page

---
+++ testscript2.js
@@ -0,0 +1,66 @@
+myitem1 = document.getElementById("bottomtext");
+
+myitem1.addEventListener("mouseover", onMouseOver);
+myitem1.addEventListener("mouseout", onMouseOut);
+
+function onMouseOver() {
+    myitem1.style.color = "aqua";
+}
+
+function onMouseOut() {
+    myitem1.style.color = "";
+}
+
+
+myitem2 = document.getElementById("bottomtext2");
+
+myitem2.addEventListener("mouseover", onMouseOver2);
+myitem2.addEventListener("mouseout", onMouseOut2);
+
+function onMouseOver2() {
+    myitem2.style.color = "aqua";
+}
+
+function onMouseOut2() {
+    myitem2.style.color = "";
+}
+
+
+myitem3 = document.getElementById("bottomtext3");
+
+myitem3.addEventListener("mouseover", onMouseOver3);
+myitem3.addEventListener("mouseout", onMouseOut3);
+
+function onMouseOver3() {
+    myitem3.style.color = "aqua";
+}
+
+function onMouseOut3() {
+    myitem3.style.color = "";
+}
+
+myitem4 = document.getElementById("mytext");
+
+myitem4.addEventListener("mouseover", onMouseOver4);
+myitem4.addEventListener("mouseout", onMouseOut4);
+
+function onMouseOver4() {
+    myitem4.style.color = "aqua";
+}
+
+function onMouseOut4() {
+    myitem4.style.color = "";
+}
+
+myitem5 = document.getElementById("contract1");
+
+myitem5.addEventListener("mouseover", onMouseOver5);
+myitem5.addEventListener("mouseout", onMouseOut5);
+
+function onMouseOver5() {
+    myitem5.style.color = "aqua";
+}
+
+function onMouseOut5() {
+    myitem5.style.color = "";
+}
